c253009e3658fc85dca512351e72b60753758e1c
Cesar Silva <ceesarf4@gmail.com>
2021-06-26 17:58:54 -0300

    Adicionando Teste Chrome

---
+++ src/test/java/cesar_qaacademy/PrimeiraClasseSelenium.java
@@ -0,0 +1,15 @@
+package cesar_qaacademy;
+
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PrimeiraClasseSelenium {
+
+	@Test
+	public void testelogin() {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com.br/");
+
+	}
+
+}
